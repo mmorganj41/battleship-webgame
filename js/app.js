b67192d563c8e5ad8b56e186939b2cf1a20d339b
mmorganj41 <mmorganj41@gmail.com>
2022-11-12 17:27:32 -0500

    fleshed out some pseudocode, altered css/html slightly for formatting and visibility

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 /* ----- Constants ----- */
 // Countdown
-// 
+// Player and computer arrays with objects indicating hit locations
 
 
 /* ----- States ----- */
-// Have player and computer arrays with objects indicating ship and hit locations
+// Turn switch and turn #
+// Hit counter
+// Ship placements
+// Setup or play bool
+// Ship orientation bool
+
 
 
 
 /* ----- Cached elements ----- */
+// Gameboards
+// Countdown
+// Ships
+// Scoreboard
+// Message
+// Orientation button
+// Fire button
+// Reset Button
+// Turn indicator
 
 const playerBoardEl = document.getElementById('playerboard');
 console.dir(playerBoardEl);
@@ -20,12 +34,22 @@ console.dir(playerBoardEl);
 
 
 /* ----- Callback Functions ------ */
-
+// Placement of player ships
+// Ship orientation button changer
+// Selection of square to fire upon
+// Firer button function - checks if square has ship and if true updates the ships on bottom
 
 
 /* ----- Main Functions ----- */
 
 // Inititalizes and resets game state
+// Updates scoreboard and message
+// Removes hit markers and previous ships
+// Assigns computer ships via randomization
+// Allows for placement of player ships
+// Forbids "firing" until all placement occurs
+// Randomly choose player turn order
+// Confirm with fire button (renamed confirm temporarily)
 
 // Render board elements:
 /*  
